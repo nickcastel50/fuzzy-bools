7a0d53de74d1d1f74bfaecc93943df45fe03d5e3
nickcastel50 <nickcastel50@gmail.com>
2023-07-24 21:07:48 -0400

    * Add EPS_MED constexpr

--- fuzzy/eps.h
+++ fuzzy/eps.h
@@ -3,6 +3,7 @@
 constexpr double EPS_NONZERO = 1e-20; // ???
 constexpr double EPS_MINISCULE = 1e-12; // what?
 constexpr double EPS_TINY = 1e-9;
+constexpr double EPS_MED = 1e-5;
 constexpr double EPS_SMALL = 1e-6;
 constexpr double EPS_BIG = 1e-4;
 constexpr double EPS_BIG2 = 1e-3;
